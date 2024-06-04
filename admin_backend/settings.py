a62de9b882c4d612521f250a2f5a23bd52060776
US <usw@info.com>
2024-06-04 16:02:39 -0700

    db password changed

--- admin_backend/settings.py
+++ admin_backend/settings.py
@@ -90,7 +90,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'nsm',
         'USER': 'postgres',
-        'PASSWORD': '123123',
+        #'PASSWORD': '123123',
+        'PASSWORD': '!QAZxsw2!',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
